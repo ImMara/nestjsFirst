79cfe08664993655adddb3dc184d23c4f68b678b
Mara <alaneraerts@live.be>
2021-03-19 22:14:13 +0100

    method to insert new products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './products.model';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  products: Product[] = [];
+  insertProduct(title: string, desc: string, price: number) {
+    const newProduct = new Product(new Date().toString(), title, desc, price);
+    this.products.push(newProduct);
+  }
+}
