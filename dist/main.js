84377a9312bb8398d54aefe58ab7c9152b768f95
Mara <alaneraerts@live.be>
2021-03-21 14:13:09 +0100

    origin allow

--- dist/main.js
+++ dist/main.js
@@ -3,7 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    var app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const options = {
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+        credentials: true,
+        allowedHeaders: 'Content-Type, Accept',
+    };
+    app.enableCors(options);
     await app.listen(3000);
 }
 bootstrap();
